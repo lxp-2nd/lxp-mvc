9664e25c5ec6eeeb06a8c92a635209455b6cc821
wlsh44 <k87913j@gmail.com>
2026-05-18 15:46:36 +0900

    init

---
+++ src/main/java/wanted/jjsbd/lxpmvc/LxpMvcApplication.java
@@ -0,0 +1,13 @@
+package wanted.jjsbd.lxpmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LxpMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LxpMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wanted/jjsbd/lxpmvc/LxpMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.jjsbd.lxpmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LxpMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
