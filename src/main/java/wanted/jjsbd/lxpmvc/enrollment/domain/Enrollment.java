e4cfcbccc7302b3cdd4dbd7761ead356fa7e3f06
minbang <idabc1@naver.com>
2026-05-19 08:56:29 +0900

    chore: checkstyle&JUnit test setup

--- src/main/java/wanted/jjsbd/lxpmvc/enrollment/domain/Enrollment.java
+++ src/main/java/wanted/jjsbd/lxpmvc/enrollment/domain/Enrollment.java
@@ -8,7 +8,13 @@ import jakarta.persistence.Id;
 @Entity
 public class Enrollment {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long enrollmentId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long enrollmentId;
+
+	// JUnit Test 코드
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
 }
