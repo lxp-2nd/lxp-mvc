0a05144439b3c4ba5f54c01c8c5f80e0aee39870
June <june.shim2@gmail.com>
2026-05-20 14:44:26 +0900

    git commit -m "refactor: MemberController 파일을 main 브랜치 초기 상태로 원상복구"

--- src/main/java/wanted/jjsbd/lxpmvc/member/controller/MemberController.java
+++ src/main/java/wanted/jjsbd/lxpmvc/member/controller/MemberController.java
@@ -2,9 +2,12 @@ package wanted.jjsbd.lxpmvc.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
 import wanted.jjsbd.lxpmvc.common.MockLxpData;
 import wanted.jjsbd.lxpmvc.member.dto.LoginRequest;
 import wanted.jjsbd.lxpmvc.member.dto.MemberProfileRequest;
@@ -28,7 +31,11 @@ public class MemberController {
 	}
 
 	@PostMapping("/login")
-	public String doLogin(LoginRequest request) {
+	public String doLogin(@Valid @ModelAttribute("loginRequest") LoginRequest request,
+		BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "member/login";
+		}
 		return "redirect:/courses";
 	}
 
