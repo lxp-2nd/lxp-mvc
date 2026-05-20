a2b83db6e94b18faaf60505d274bef85f551d5db
June <june.shim2@gmail.com>
2026-05-20 09:40:40 +0900

    feat: 식별자 매핑 기반 MethodArgumentNotValidException 핸들러 구현

--- src/main/java/wanted/jjsbd/lxpmvc/common/exception/GlobalExceptionHandler.java
+++ src/main/java/wanted/jjsbd/lxpmvc/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package wanted.jjsbd.lxpmvc.common.exception;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;	
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,4 +23,19 @@ public class GlobalExceptionHandler {
 		log.error(e.getMessage(), e);
 		return "/";
 	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleValidationException(MethodArgumentNotValidException e, RedirectAttributes redirectAttributes) {
+        try {
+            String errorTarget = e.getBindingResult().getFieldError().getDefaultMessage();
+            ErrorCode errorCode = ErrorCode.valueOf(errorTarget); 
+            redirectAttributes.addAttribute("error", true);
+            redirectAttributes.addAttribute("message", errorCode.getMessage()); 
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            redirectAttributes.addAttribute("error", true);
+            redirectAttributes.addAttribute("message", ErrorCode.INVALID_INPUT.getMessage());
+        }
+        return "redirect:/login";
+    }
 }
