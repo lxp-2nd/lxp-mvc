3e7355ba86cea3c26d93bf6241590ad2dad1ce5d
June <june.shim2@gmail.com>
2026-05-20 14:40:07 +0900

    refactor: SecurityConfig 내 CSRF 비활성화 설정을 메서드 참조 형식으로 변경

--- src/main/java/wanted/jjsbd/lxpmvc/config/SecurityConfig.java
+++ src/main/java/wanted/jjsbd/lxpmvc/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -16,12 +17,12 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.csrf(csrf -> csrf.disable())
+			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/login", "/signup", "/courses", "/courses/{courseId}").permitAll()
 				.anyRequest().authenticated()
 			)
-			.formLogin(form -> form.disable());
+			.formLogin(AbstractHttpConfigurer::disable);
 		return http.build();
 	}
 }
