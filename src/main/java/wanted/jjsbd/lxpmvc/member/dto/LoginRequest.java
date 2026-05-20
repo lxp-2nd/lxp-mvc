801e596d7369803f6734feb8edb07ecfad601836
June <june.shim2@gmail.com>
2026-05-20 10:10:54 +0900

    feat: LoginRequest DTO 작성

--- src/main/java/wanted/jjsbd/lxpmvc/member/dto/LoginRequest.java
+++ src/main/java/wanted/jjsbd/lxpmvc/member/dto/LoginRequest.java
@@ -1,9 +1,17 @@
 package wanted.jjsbd.lxpmvc.member.dto;
 
-public record LoginRequest(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
-	public LoginRequest {
-		email = email == null ? "" : email;
-		password = password == null ? "" : password;
-	}
+@Getter @Setter
+public class LoginRequest {
+
+	@NotBlank(message = "MEMBER_REQUIRED_EMAIL")
+	@Email(message = "MEMBER_INVALID_EMAIL_FORMAT")
+	private String username;
+
+	@NotBlank(message = "MEMBER_REQUIRED_PASSWORD")
+	private String password;
 }
