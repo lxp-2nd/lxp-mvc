3b227a3936ae378d5efb1fc0073e9d04407d6326
June <june.shim2@gmail.com>
2026-05-20 16:42:19 +0900

    feat: MembeãrRole 구현

---
+++ src/main/java/wanted/jjsbd/lxpmvc/member/domain/MemberRole.java
@@ -0,0 +1,17 @@
+package wanted.jjsbd.lxpmvc.member.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum MemberRole {
+
+	BASIC("ROLE_BASIC"),
+	INSTRUCTOR("ROLE_INSTRUCTOR"),
+	ADMIN("ROLE_ADMIN");
+
+	private final String securityCode;
+
+	MemberRole(String securityCode) {
+		this.securityCode = securityCode;
+	}
+}
