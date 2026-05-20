a95c9d0fc7dac5d41a4a5a87b3b4118811b2ed9d
June <june.shim2@gmail.com>
2026-05-20 10:11:17 +0900

    feat: LoginRequest DTO 작성

--- src/main/java/wanted/jjsbd/lxpmvc/member/dto/LoginRequest.java
+++ src/main/java/wanted/jjsbd/lxpmvc/member/dto/LoginRequest.java
@@ -5,7 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 public class LoginRequest {
 
 	@NotBlank(message = "MEMBER_REQUIRED_EMAIL")
