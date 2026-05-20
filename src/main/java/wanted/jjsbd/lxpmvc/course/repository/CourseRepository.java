c1c2d04fa45ce319e3987aa3b882b30da4e94e74
J-DONGHYUN <dongza8101@naver.com>
2026-05-20 19:55:39 +0900

    feat: repository 생성

---
+++ src/main/java/wanted/jjsbd/lxpmvc/course/repository/CourseRepository.java
@@ -0,0 +1,13 @@
+package wanted.jjsbd.lxpmvc.course.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import wanted.jjsbd.lxpmvc.course.domain.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Integer> {
+
+	/// 제목(Title)에 검색어가 포함되어 있고, 활성화된 강의 목록을 페이징/정렬하여 조회
+	Page<Course> findByTitleContaining(String title, Pageable pageable);
+}
