2e39f378374298fb4d073ec7db0beac2a9fc6573
wlsh44 <k87913j@gmail.com>
2026-05-20 17:32:05 +0900

    fix: 강의 목록 수강생 수 빠진 부분 추가

--- src/main/java/wanted/jjsbd/lxpmvc/course/dto/CourseResponse.java
+++ src/main/java/wanted/jjsbd/lxpmvc/course/dto/CourseResponse.java
@@ -3,5 +3,5 @@ package wanted.jjsbd.lxpmvc.course.dto;
 import java.util.List;
 
 public record CourseResponse(String id, String title, String instructor, String description,
-		List<SectionResponse> curriculum) {
+		List<SectionResponse> curriculum, int learnerCount) {
 }
