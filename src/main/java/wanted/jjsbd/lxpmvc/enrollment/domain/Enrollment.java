29c445fa1cf8dfeb3a36a16ebe8324c0402fbbef
minbang <idabc1@naver.com>
2026-05-20 14:34:23 +0900

    feat:entity id 규칙 적용

--- src/main/java/wanted/jjsbd/lxpmvc/enrollment/domain/Enrollment.java
+++ src/main/java/wanted/jjsbd/lxpmvc/enrollment/domain/Enrollment.java
@@ -34,15 +34,15 @@ public class Enrollment extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "enrollment_id")
-	private Long enrollmentId;
+	@Column(name = "id")
+	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "member_id", nullable = false)
+	@JoinColumn(name = "id", nullable = false)
 	private Member member;
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "course_id", nullable = false)
+	@JoinColumn(name = "id", nullable = false)
 	private Course course;
 
 	private Enrollment(Member member, Course course) {
