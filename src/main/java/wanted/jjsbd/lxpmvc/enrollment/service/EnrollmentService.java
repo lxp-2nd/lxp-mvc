c8d4f02e9e11f35c68d9513fed73f26867a419c6
minbang <idabc1@naver.com>
2026-05-22 06:40:50 +0900

    fix:service에서 회원 및 강의 repository로 조회 적용

--- src/main/java/wanted/jjsbd/lxpmvc/enrollment/service/EnrollmentService.java
+++ src/main/java/wanted/jjsbd/lxpmvc/enrollment/service/EnrollmentService.java
@@ -7,21 +7,29 @@ import jakarta.transaction.Transactional;
 import wanted.jjsbd.lxpmvc.common.exception.CustomException;
 import wanted.jjsbd.lxpmvc.common.exception.ErrorCode;
 import wanted.jjsbd.lxpmvc.course.domain.Course;
+import wanted.jjsbd.lxpmvc.course.repository.CourseRepository;
 import wanted.jjsbd.lxpmvc.enrollment.domain.Enrollment;
 import wanted.jjsbd.lxpmvc.enrollment.dto.EnrollmentRequest;
 import wanted.jjsbd.lxpmvc.enrollment.repository.EnrollmentRepository;
 import wanted.jjsbd.lxpmvc.member.domain.Member;
+import wanted.jjsbd.lxpmvc.member.repository.MemberRepository;
 
 @Service
 public class EnrollmentService {
 
 	private final EnrollmentRepository enrollmentRepository;
-	// private final CourseRepository courseRepository;
-	// private final MemberRepository memberRepository;
+	private final CourseRepository courseRepository;
+	private final MemberRepository memberRepository;
 
 	// 생성자 패턴(@Autowired 제거)
-	public EnrollmentService(EnrollmentRepository enrollmentRepository) {
+	public EnrollmentService(
+		EnrollmentRepository enrollmentRepository,
+		MemberRepository memberRepository,
+		CourseRepository courseRepository
+	) {
 		this.enrollmentRepository = enrollmentRepository;
+		this.memberRepository = memberRepository;
+		this.courseRepository = courseRepository;
 	}
 
 	// 수강신청(DB 저장)
@@ -44,18 +52,14 @@ public class EnrollmentService {
 		}
 
 		// 회원 조회
-		// memberRepository 구현 후 추후 추가 수정
 		// ErrorCode 상황: 회원이 회원ID로 조회되지 않는 경우
-		// Member learner = memberRepository.findById(request.learnerId())
-		// 	.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT));
-		Member learner = new Member();
+		Member learner = memberRepository.findById(request.learnerId())
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT));
 
 		// 강의 조회
-		// courseRepository 구현 후 추후 추가 수정
 		// ErrorCode 상황: 강의가 강의ID로 조회되지 않는 경우
-		// Course course = courseRepository.findById(request.courseId())
-		// 	.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT));
-		Course course = Course.createCourse(learner, "title1", "설명설명");
+		Course course = courseRepository.findById(request.courseId())
+			.orElseThrow(() -> new CustomException(ErrorCode.INVALID_INPUT));
 
 		Enrollment enrollment = Enrollment.createEnrollment(learner, course);
 
