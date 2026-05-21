bf56dcc5f5a02e65b0f30fcd46860db0e5604cce
J-DONGHYUN <dongza8101@naver.com>
2026-05-21 17:33:34 +0900

    refactor: checkstyle 수정

--- src/test/java/wanted/jjsbd/lxpmvc/course/repository/CourseRepositoryTest.java
+++ src/test/java/wanted/jjsbd/lxpmvc/course/repository/CourseRepositoryTest.java
@@ -12,11 +12,15 @@ import org.springframework.data.domain.PageRequest;
 
 import wanted.jjsbd.lxpmvc.course.domain.Course;
 import wanted.jjsbd.lxpmvc.member.domain.Member;
+import wanted.jjsbd.lxpmvc.member.repository.MemberRepository;
 
 @DataJpaTest //h2 인메모리 사용.
 class CourseRepositoryTest {
 	@Autowired
 	private CourseRepository courseRepository;
+
+	@Autowired
+	private MemberRepository memberRepository;
 
 	@Autowired
 	private TestEntityManager em;
@@ -26,17 +30,14 @@ class CourseRepositoryTest {
 	void findByTitleContainingAndDeletedAtIsNull() {
 
 		//given
-		Member instructor = new Member();
-		em.persist(instructor);
+		Member instructor = Member.createBasicMember("일타강사", "instructor@example.com", "dummyPassword123!");
+		memberRepository.save(instructor);
 
 		Course course1 = Course.createCourse(instructor, "서비스 기획 MVP", "서비스 기획의 기초를 배웁니다.");
 		Course course2 = Course.createCourse(instructor, "스프링 부트 입문", "백엔드 개발의 기초를 배웁니다.");
-		em.persist(course1);
-		em.persist(course2);
 
-		// 영속성 컨택스트의 변경 사항을 DB에 반영하고 비워서, 캐시가 아닌 실제 쿼리가 나가는지 확인
-		em.flush();
-		em.clear();
+		courseRepository.save(course1);
+		courseRepository.save(course2);
 
 		// 첫번째 페이지(0), 페이지당 10개씩 가져오는 Pageable 객체 생성
 		PageRequest pageRequest = PageRequest.of(0, 10);
