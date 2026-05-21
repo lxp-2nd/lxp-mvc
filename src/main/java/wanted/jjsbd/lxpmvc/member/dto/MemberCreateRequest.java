3a98ea2781be0e15e90f60e31914cfea78ac3d3e
June <june.shim2@gmail.com>
2026-05-21 12:15:44 +0900

    feat: MemberCreateRequest 구현

---
+++ src/main/java/wanted/jjsbd/lxpmvc/member/dto/MemberCreateRequest.java
@@ -0,0 +1,21 @@
+package wanted.jjsbd.lxpmvc.member.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record MemberCreateRequest(
+	@NotBlank(message = "닉네임은 필수 입력 항목입니다.")
+	@Size(max = 20, message = "닉네임은 최대 20자까지 가능합니다.")
+	String nickname,
+
+	@NotBlank(message = "이메일은 필수 입력 항목입니다.")
+	@Size(max = 100, message = "이메일은 최대 100자까지 가능합니다.")
+	@Email(message = "올바르지 않은 이메일 형식입니다.")
+	String email,
+
+	@NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+	@Size(min = 4, max = 20, message = "비밀번호는 4자 이상 20자 이하여야 합니다.")
+	String password
+) {
+}
