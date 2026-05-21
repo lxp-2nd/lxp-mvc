917903add45df5502b3b05a9b584b257f2670026
J-DONGHYUN <dongza8101@naver.com>
2026-05-21 09:39:54 +0900

    refactor: 제목검색으로만 찾는 필터링 -> 추가로 deletedAt 필터링을 추가

--- src/main/java/wanted/jjsbd/lxpmvc/course/repository/CourseRepository.java
+++ src/main/java/wanted/jjsbd/lxpmvc/course/repository/CourseRepository.java
@@ -9,5 +9,5 @@ import wanted.jjsbd.lxpmvc.course.domain.Course;
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
 	/// 제목(Title)에 검색어가 포함되어 있고, 활성화된 강의 목록을 페이징/정렬하여 조회
-	Page<Course> findByTitleContaining(String title, Pageable pageable);
+	Page<Course> findByTitleContainingAndDeletedAtIsNull(String title, Pageable pageable);
 }
