6849a5ef6dddce00a0a68aa3f9aa040b05a337a8
J-DONGHYUN <dongza8101@naver.com>
2026-05-21 15:48:53 +0900

    fix: 수강생 필드 이름 html 바인딩에 맞게 수정

--- src/main/java/wanted/jjsbd/lxpmvc/course/dto/CourseResponse.java
+++ src/main/java/wanted/jjsbd/lxpmvc/course/dto/CourseResponse.java
@@ -7,7 +7,7 @@ public record CourseResponse(
 	String title, /// 강의명
 	String instructor,
 	String description,
-	Integer leanerCount,
+	Integer learnerCount,
 	List<SectionResponse> curriculum ///하위 커리큘럼(섹션+자료) 목록
 
 ) {
