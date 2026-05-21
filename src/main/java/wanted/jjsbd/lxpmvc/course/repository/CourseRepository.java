585e73ffef5fbc31ee6225eae9ec30be0a7d5575
J-DONGHYUN <dongza8101@naver.com>
2026-05-21 18:50:43 +0900

    feat: N+1 문제로 JPQL을 사용하여 커스텀 메서드를 만듬

--- src/main/java/wanted/jjsbd/lxpmvc/course/repository/CourseRepository.java
+++ src/main/java/wanted/jjsbd/lxpmvc/course/repository/CourseRepository.java
@@ -1,13 +1,21 @@
 package wanted.jjsbd.lxpmvc.course.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import wanted.jjsbd.lxpmvc.course.domain.Course;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-	/// 제목(Title)에 검색어가 포함되어 있고, 활성화된 강의 목록을 페이징/정렬하여 조회
 	Page<Course> findByTitleContainingAndDeletedAtIsNull(String title, Pageable pageable);
+
+	@EntityGraph(attributePaths = {"sections"})
+	@Query("SELECT c FROM Course c WHERE c.id = :id and c.deletedAt is null ")
+	Optional<Course> findByIdWithCurriculum(@Param("id") String id);
 }
