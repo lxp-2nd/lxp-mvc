f289f3b3bfa0173553c158c45c8e25f290a53737
June <june.shim2@gmail.com>
2026-05-22 10:26:51 +0900

    feat: SecuritySessionManager 으로 스프링 시큐리티 세션 인증 객체 생성 및 세션 고정 공격 방어 공통 메서드 묶음

---
+++ src/main/java/wanted/jjsbd/lxpmvc/config/security/SecuritySessionManager.java
@@ -0,0 +1,24 @@
+package wanted.jjsbd.lxpmvc.config.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import wanted.jjsbd.lxpmvc.member.domain.AuthInfo;
+
+@Component
+public class SecuritySessionManager {
+
+	public void loginAndSyncSession(AuthInfo authInfo, HttpServletRequest servletRequest) {
+		UsernamePasswordAuthenticationToken authenticationToken =
+			new UsernamePasswordAuthenticationToken(authInfo, null, authInfo.getAuthorities());
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		securityContext.setAuthentication(authenticationToken);
+		servletRequest.changeSessionId();
+		HttpSession session = servletRequest.getSession(true);
+		session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+	}
+}
