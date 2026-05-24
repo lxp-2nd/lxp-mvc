5468f7e75fd559748884bf1521d53941a741d342
June <june.shim2@gmail.com>
2026-05-25 03:51:58 +0900

    feat: get profile dto 구현

--- src/main/java/wanted/jjsbd/lxpmvc/member/dto/MemberResponse.java
+++ src/main/java/wanted/jjsbd/lxpmvc/member/dto/MemberResponse.java
@@ -1,4 +1,10 @@
 package wanted.jjsbd.lxpmvc.member.dto;
 
-public record MemberResponse(String name, String email) {
+import wanted.jjsbd.lxpmvc.member.domain.Member;
+
+public record MemberResponse(String nickname, String email) {
+
+	public static MemberResponse from(Member member) {
+		return new MemberResponse(member.getNickname(), member.getEmail());
+	}
 }
